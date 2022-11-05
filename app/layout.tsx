ba822a0745039dcd144dc255633b806a8e2a2a09
Gil Alves <gildson_af@outlook.com>
2022-11-04 22:38:59 -0300

    (fix hydration) script theme change into header tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Work_Sans } from '@next/font/google';
 import ToggleTheme from '../components/ToggleTheme';
-import './globals.css';
+import '../styles/index.scss';
 
 const workSans = Work_Sans();
 
@@ -10,19 +10,21 @@ interface LayoutRootProps {
 
 export default function RootLayout({ children }: LayoutRootProps) {
 	return (
-		<html className={`dark ${workSans.className}`}>
-			<body>
-				<section className="h-screen px-9 flex flex-col justify-start">
-					<header className="h-10 flex justify-end">
+		<>
+			<html className={`dark ${workSans.className}`}>
+				<body className="min-h-full px-9 flex flex-col justify-start">
+					<div className="h-10 flex justify-end">
 						<ToggleTheme />
-					</header>
-					<main className="h-full border p-10 border-slate-500">{children}</main>
+					</div>
+					<main className="h-full scrollbar overflow-y-scroll border p-10 border-slate-500/10">
+						{children}
+					</main>
 
-					<footer className="select-none h-10 text-sm opacity-70 mt-auto grid place-items-center">
+					<footer className="mt-auto select-none py-2 text-center text-sm opacity-70 ">
 						Designed and coded by Gildson Alves © 2022 (W.I.P.)
 					</footer>
-				</section>
-			</body>
-		</html>
+				</body>
+			</html>
+		</>
 	);
 }
