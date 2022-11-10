4f05e5678f18b4e49d949e52b193254559db05a2
Gil Alves <gildson_af@outlook.com>
2022-11-10 11:16:15 -0300

    (refact seo) main header component initial dynamic results

--- app/head.tsx
+++ app/head.tsx
@@ -1,15 +1,23 @@
-export default async function Head() {
+import { client } from '../libs/graphql/client';
+import { getOwnerSEO } from '../libs/graphql/queries/owner';
+
+const Head = async () => {
+	const {
+		owners: [owner],
+	} = await client.request(getOwnerSEO);
+
+	const { title, description, keywords } = owner.seo;
+
 	return (
 		<>
-			<title>Gildson Alves</title>
 			<meta charSet="UTF-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<meta name="author" content="Gildson Alves - Front-end Developer" />
-			<meta name="description" content="Este é meu portifolio, vem dar uma olhada nos meus trabalhos" />
-			<meta
-				name="keywords"
-				content="Portifolio, Front-end, Developer, frontend, CSS, JavaScript, HTML, React, Typescript, Next, Next.js"
-			></meta>
+			<title>{title}</title>
+			<meta name="author" content="Gildson Alves" />
+			<meta name="description" content={description} />
+			<meta name="keywords" content={keywords} />
 		</>
 	);
-}
+};
+
+export default Head;
