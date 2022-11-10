b4e67b862154c1e05b5f56798090528ed7938b51
Gil Alves <gildson_af@outlook.com>
2022-11-10 11:18:54 -0300

    (refact page) about dynamic and external image config

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,41 +1,35 @@
 import Navigator from '../../components/Navigator';
 import Title from '../../components/Title';
+import { client } from '../../libs/graphql/client';
+import { getOwnerAbout, getOwnerHeading } from '../../libs/graphql/queries/owner';
 import { AboutImage } from './styles';
 
-const About = () => {
+const About = async () => {
+	const {
+		owners: [heading],
+	} = await client.request(getOwnerHeading);
+	const {
+		owners: [about],
+	} = await client.request(getOwnerAbout);
+
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 gap-32 h-full">
 			<div className="flex flex-col justify-between">
-				<Title />
+				<Title name={heading?.name} subtitle={heading?.subtitle} />
 				<Navigator />
 			</div>
+
 			<div className="overflow-y-scroll scrollbar lg:py-52 -my-10">
 				<figure className="bg-primary-500/80 relative mx-auto mb-10 w-80 h-80">
 					<AboutImage
-						src={'/AboutMe.jpg'}
+						src={about.image.url}
 						alt="Gildson Foto"
 						fill
 						priority
 						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
 					/>
 				</figure>
-				<div className="rich-text flex flex-col items-center gap-4">
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolor, ut consequuntur quis,
-						numquam molestiae esse <a>doloremque</a> reiciendis obcaecati, nemo quae Doloribus deserunt ut
-						iure culpa aperiam iste cumque autem?
-					</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolor, ut consequuntur quis,
-						numquam molestiae esse <a>doloremque</a> reiciendis obcaecati, nemo quae! Doloribus deserunt ut
-						iure culpa aperiam iste cumque autem?
-					</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolor, ut consequuntur quis,
-						numquam molestiae esse <a>doloremque</a> reiciendis obcaecati, nemo quae Doloribus deserunt ut
-						iure culpa aperiam iste cumque autem?
-					</p>
-				</div>
+				<div className="rich-text flex flex-col items-center gap-4">{about.description.html}</div>
 			</div>
 		</div>
 	);
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	images: {
+		domains: ['media.graphassets.com'],
+	},
 	experimental: {
 		appDir: true,
 		fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
