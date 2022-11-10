0c43ad6045f77f9dbd59fe1cd99b86ae80fe3afa
Gil Alves <gildson_af@outlook.com>
2022-11-10 11:18:12 -0300

    (refact pages) dynamic results to contacts and works

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,26 +1,28 @@
 import Navigator from '../../components/Navigator';
 import Title from '../../components/Title';
 import WorkCard from '../../components/WorkCard';
+import { client } from '../../libs/graphql/client';
+import { getOwnerHeading, getOwnerWorks } from '../../libs/graphql/queries/owner';
 
-const Work = () => {
+const Work = async () => {
+	const {
+		owners: [heading],
+	} = await client.request(getOwnerHeading);
+
+	const {
+		owners: [owner],
+	} = await client.request(getOwnerWorks);
+
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 gap-32 h-full">
 			<div className="flex flex-col justify-between">
-				<Title />
+				<Title name={heading?.name} subtitle={heading?.subtitle} />
 				<Navigator />
 			</div>
 			<div className="flex flex-col overflow-y-scroll scrollbar -my-10 py-52 gap-8">
-				<WorkCard link="/work/test1" />
-				<WorkCard link="/work/test2" />
-				<WorkCard link="/work/test3" />
-				<WorkCard link="/work/test4" />
-				<WorkCard link="/work/test1" />
-				<WorkCard link="/work/test2" />
-				<WorkCard link="/work/test3" />
-				<WorkCard link="/work/test4" />
-				<WorkCard link="/work/test1" />
-				<WorkCard link="/work/test2" />
-				<WorkCard link="/work/test3" />
+				{owner.works.map(({ slug }) => (
+					<WorkCard key={slug} link={`/work/${slug}`} />
+				))}
 			</div>
 		</div>
 	);
