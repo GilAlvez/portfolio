ef1e116c6fd87d8a7fc2ee8029d3356707165378
Gil Alves <gildson_af@outlook.com>
2022-11-11 16:10:51 -0300

    (feat component) page animations, loading and fade in

---
+++ components/Animate/PageAnimation.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+const PageAnimation = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<motion.div
+			className="h-full"
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 0.5 }}
+		>
+			{children}
+		</motion.div>
+	);
+};
+
+export default PageAnimation;
