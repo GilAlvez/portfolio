5fc9e3969d86dd76281fdb9a43749b500b637a3f
Gil Alves <gildson_af@outlook.com>
2022-11-12 09:53:55 -0300

    (fix page) revert wrapper client component until layout children dont have a wrapper inside

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable @next/next/no-img-element */
 import { Exo } from '@next/font/google';
 import BackgroundAnimation from '../components/BackgroundAnimation';
-import Main from '../components/Main';
 import NoiseFilter from '../components/NoiseFilter';
 import ToggleTheme from '../components/ToggleTheme';
 import '../styles/index.scss';
@@ -9,11 +7,8 @@ import '../styles/index.scss';
 export const revalidate = 3600;
 
 const font = Exo();
-interface LayoutRootProps {
-	children: React.ReactNode;
-}
 
-export default function RootLayout({ children }: LayoutRootProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<html className={`${font.className} dark`}>
@@ -22,7 +17,10 @@ export default function RootLayout({ children }: LayoutRootProps) {
 						<ToggleTheme />
 					</div>
 					<BackgroundAnimation />
-					<Main>{children}</Main>
+					<main className="h-full backdrop-blur-3xl lg:backdrop-blur-[90px] backdrop-brightness-[1.15] dark:backdrop-brightness-50 scrollbar overflow-y-scroll border rounded-md p-10 border-zinc-500/50">
+						{/* <AnimatePresence mode="wait">{children}</AnimatePresence> */}
+						{children}
+					</main>
 					<footer className="mt-auto select-none py-2 text-center font-normal text-sm opacity-90">
 						Designed and coded by Gildson Alves © 2022 (W.I.P.)
 					</footer>
