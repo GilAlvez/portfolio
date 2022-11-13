7e5d6fe7793026fede0f5e16a876685791902160
Gil Alves <gildson_af@outlook.com>
2022-11-13 18:16:56 -0300

    (styles mobile) navigation same spacing between content and title

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,8 +16,8 @@ const About = async () => {
 
 	return (
 		<PageAnimation>
-			<div className="grid grid-cols-1 gap-16 lg:h-full lg:gap-32 md:grid-cols-2">
-				<div className="flex flex-col gap-10 lg:justify-between">
+			<div className="grid grid-cols-1 gap-14 lg:h-full lg:gap-32 md:grid-cols-2">
+				<div className="flex flex-col gap-14 lg:justify-between">
 					<Title name={heading?.name} subtitle={heading?.subtitle} />
 					<Navigator />
 				</div>
--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ const Page = async () => {
 
 	return (
 		<PageAnimation>
-			<div className="grid grid-cols-1 gap-16 lg:h-full lg:gap-32 md:grid-cols-2">
-				<div className="flex flex-col gap-10 lg:justify-between">
+			<div className="grid grid-cols-1 gap-14 lg:h-full lg:gap-32 md:grid-cols-2">
+				<div className="flex flex-col gap-14 lg:justify-between">
 					<Title name={owner?.name} subtitle={owner?.subtitle} />
 					<Navigator />
 				</div>
