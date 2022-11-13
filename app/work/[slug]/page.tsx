815710c87cc7c030023bab8800b2fcc6896e222a
Gil Alves <gildson_af@outlook.com>
2022-11-13 18:50:22 -0300

    (styles mobile) component subtitle table and mobile layout

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { getWorkBySlug, getWorksSlugs } from '../../../libs/graphql/queries/work
 
 import sanitizeHtml from 'sanitize-html';
 import PageAnimation from '../../../components/Animate/PageAnimation';
+import SubtitlesTable from '../../../components/SubtitlesTable';
 
 export const dynamicParams = false;
 
@@ -14,7 +15,7 @@ const WorkBySlug = async ({ params }: { params: { slug: string } }) => {
 
 	return (
 		<PageAnimation>
-			<figure className="-mx-10 -mt-10 h-40 relative select-none">
+			<figure className="relative h-40 -mx-10 -mt-10 select-none">
 				<Image
 					src={work?.banner.url as string}
 					alt="Banner do projeto"
@@ -23,43 +24,35 @@ const WorkBySlug = async ({ params }: { params: { slug: string } }) => {
 					priority
 				/>
 			</figure>
-			<div className="flex flex-col gap-8 py-8 container mx-auto lg:px-32 2xl:px-52">
+			<div className="container flex flex-col gap-8 py-8 mx-auto lg:px-32 2xl:px-52">
 				<h1>{work?.title}</h1>
 				<hr />
-				<section className="grid grid-cols-2 gap-8">
-					<table className="table-auto w-fit">
-						<tbody>
-							<tr className="align-top ">
-								<td className="pr-6 pb-6 font-semibold">Lançamento</td>
-								<td className="pr-6 pb-6">{work?.release}</td>
-							</tr>
-							<tr className="align-top ">
-								<td className="pr-6 pb-6 font-semibold">Atribuições</td>
-								<td className="pr-6 pb-6">{work?.responsibilities.map((item) => `${item}, `)}</td>
-							</tr>
-							<tr className="align-top ">
-								<td className="font-semibold">Tecnologias</td>
-								<td>{work?.techs.map((item) => `${item}, `)}</td>
-							</tr>
-						</tbody>
-					</table>
+				<section className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+					<SubtitlesTable
+						release={work?.release}
+						responsibilities={work?.responsibilities}
+						techs={work?.techs}
+					/>
+
 					<div className="flex flex-col gap-6">
 						<p>{work?.resume}</p>
-						<a
-							className="text-lg flex items-center gap-2 w-max"
-							href={work?.link as any}
-							target="_blank"
-							rel="noreferrer noopener"
-						>
-							<span>{work?.link}</span>
-							<BsArrowUpRight />
-						</a>
+						{work?.link && (
+							<a
+								className="flex items-center gap-2 w-max"
+								href={work.link}
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								<span>Visualizar o site</span>
+								<BsArrowUpRight />
+							</a>
+						)}
 					</div>
 				</section>
 				<hr />
 
 				<section
-					className="rich-text flex flex-col items-center gap-8"
+					className="flex flex-col items-center gap-8 rich-text"
 					dangerouslySetInnerHTML={{ __html: sanitizeHtml(work?.content.html as string) }}
 				/>
 				<span className="text-end">{new Date(work?.updatedAt).toLocaleString()}</span>
