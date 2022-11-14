c68ce308ca5bb64e14798ff1a2c5b1aaf7529ba4
Gil Alves <gildson_af@outlook.com>
2022-11-14 17:02:58 -0300

    (feat component) chip tag and conditional link to active works prop

---
+++ components/Chip/index.tsx
@@ -0,0 +1,16 @@
+import { ChipStyled } from './styles';
+
+interface ChipProps extends React.HTMLAttributes<HTMLSpanElement> {
+	color: 'primary' | 'secondary' | 'green' | 'blue' | 'yellow';
+	children: React.ReactNode;
+}
+
+const Chip = ({ color, children, ...props }: ChipProps) => {
+	return (
+		<ChipStyled $color={color} {...props}>
+			{children}
+		</ChipStyled>
+	);
+};
+
+export default Chip;
