b50c81f7c22c62d4895d9ca6982d3768ec33efc1
Gil Alves <gildson_af@outlook.com>
2022-11-14 14:20:03 -0300

    (styles component) toggle theme change icon

--- components/ToggleTheme/index.tsx
+++ components/ToggleTheme/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { CgDarkMode } from 'react-icons/cg';
+import { RiContrast2Fill } from 'react-icons/ri';
 
 const ToggleTheme = () => {
 	const handleTheme = () => {
@@ -8,8 +8,8 @@ const ToggleTheme = () => {
 	};
 
 	return (
-		<button className="outline-0 p-2 text-xl hover:bg-zinc-500/40" onClick={handleTheme}>
-			<CgDarkMode />
+		<button className="p-2 text-xl outline-0 hover:bg-zinc-500/40" onClick={handleTheme}>
+			<RiContrast2Fill />
 		</button>
 	);
 };
