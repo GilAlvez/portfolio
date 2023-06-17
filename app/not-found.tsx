62fdff0830c532a4ece55f931ff6d00cecdd9f5a
Gildson Alves <gildson_af@outlook.com>
2023-06-17 10:57:19 -0300

    refact: removes button in 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next';
-import Link from 'next/link';
 
 export const metadata: Metadata = {
 	title: '404 - Página não encontrada',
@@ -7,12 +6,9 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
 	return (
-		<section className="grid place-items-center">
-			<h1>404</h1>
-			<h2>Página não encontrada</h2>
-			<Link className="px-4 py-2 mt-12 text-xl text-white rounded bg-primary-600" href="/">
-				Voltar para a tela inicial
-			</Link>
+		<section className="flex flex-col items-center justify-center h-full">
+			<h2 className="text-9xl">404</h2>
+			<h3>Página não encontrada</h3>
 		</section>
 	);
 }
